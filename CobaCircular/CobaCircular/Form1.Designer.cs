24620fc16ffc30b10ab7ce34f87d8a89e1c2b95b
Dimas Syahputra <37606879+mikazu48@users.noreply.github.com>
2020-01-09 13:46:08 +0700

    Add files via upload

---
+++ CobaCircular/CobaCircular/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace CobaCircular
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCirc1 = new System.Windows.Forms.TextBox();
+            this.txtCircResult = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 43);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(488, 276);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Go Circular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(104, 17);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Much Circular";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 332);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Real Temp & Result";
+            // 
+            // txtCirc1
+            // 
+            this.txtCirc1.Location = new System.Drawing.Point(310, 330);
+            this.txtCirc1.Name = "txtCirc1";
+            this.txtCirc1.Size = new System.Drawing.Size(53, 20);
+            this.txtCirc1.TabIndex = 4;
+            // 
+            // txtCircResult
+            // 
+            this.txtCircResult.Location = new System.Drawing.Point(380, 330);
+            this.txtCircResult.Name = "txtCircResult";
+            this.txtCircResult.Size = new System.Drawing.Size(53, 20);
+            this.txtCircResult.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 378);
+            this.Controls.Add(this.txtCircResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCirc1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Circular Simulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCirc1;
+        private System.Windows.Forms.TextBox txtCircResult;
+    }
+}
+
---
+++ CobaCircular/CobaCircular/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CobaCircular
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            p_GenerateCircular();
+        }
+        private void p_GenerateCircular()
+        {
+            try
+            {
+                int iTotalCirc = int.Parse(textBox2.Text);
+
+                decimal decSumEnd = 0;
+
+                decimal decSumRest = 0;
+                decimal decSum = 0;
+                decimal decFix = 0;
+                decimal decFinalRes = 0;
+                decimal decRestFix = 0;
+                bool bCirc = false;
+                for (int i = 0; i < iTotalCirc; i++)
+                {
+                    if(!bCirc)
+                    {
+                        foreach (DataRow dr in dt.Rows)
+                        {
+                            decFix += decimal.Parse(dr[0].ToString());
+                        }
+                        decSum = decFix;
+                        bCirc = true;
+                    }
+                    if(decSumRest == 0)
+                    {
+                        decSumRest = (decSum * 5) / 100; // ini karena 5%
+                        decSumEnd = decFix + decSumRest;
+                    }
+                    else
+                    {
+                        decSum = decSumEnd;
+
+                        decSumRest = (decSumRest * 5) / 100;
+                        decSumEnd += decSumRest;
+
+                        //For perline
+                        decRestFix += decSumRest;
+
+                        decFinalRes = (decimal)decSumEnd - (decimal)decSum;
+                        if (decFinalRes <= 0.001m)
+                        {
+                            break;
+                        }
+                    }
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        dr[1] = decimal.Parse(dr[0].ToString()) + decRestFix;
+                    }
+
+                }
+
+                txtCirc1.Text = decSum.ToString();
+                txtCircResult.Text = (decSumEnd).ToString();
+                string szMsg = "The difference of circ " + decFinalRes.ToString();
+                MessageBox.Show(szMsg);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }  
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            dt.Columns.Add("Number");
+            dt.Columns.Add("Number 2");
+
+            DataRow dr = dt.NewRow();
+            dr[0] = 10;
+            dr[1] = 0;
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr[0] = 20;
+            dr[1] = 0;
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr[0] = 30;
+            dr[1] = 0;
+            dt.Rows.Add(dr);
+            
+            dataGridView1.DataSource = dt;
+
+        }
+    }
+}
